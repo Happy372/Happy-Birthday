48a1181c903cbfca7a08218687ea35f6baed7440
Happy372 <pmarygrace300@gmail.com>
2025-03-21 00:42:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+document.getElementById("birthdayCard").addEventListener("click", function () {
+    this.style.transform = this.style.transform === "rotateY(180deg)" ? "rotateY(0deg)" : "rotateY(180deg)";
+});
